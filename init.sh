f1c8a95afa4f9d50ebdecc636411371f23103410
ViniciusJO <viniciusjosedeoliveira@outlook.com>
2024-06-23 15:56:07 -0300

    init.sh and font zsh

---
+++ init.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/sh
+
+sudo pacman -S git zsh stow
+
+stow bob/ fonts/ i3/ nchat/ ncspot/ nvim/ octave/ pip/ rofi/ scripts/ sound_effects/ tmux/ wallpapers/ wezterm/ zsh/
+
+chsh -s /usr/bin/zsh
+
+REQUIRED_PACMAN_PACKAGES="ardour bat bob brave calf curl dolphin dragonfly-bin eza fd firefox fzf gcc gdb guitarix gx i3 lazygit lua luarocks ly maim make mesa mesa-demos mesa-utils nano onlyoffice opera pacman pavucontrol pipewire pipewire-alsa pipewire-autostart pipewire-pulse pipewire-zeroconf qpwgraph ripgrep steam sublime-text-4 thefuck tmux vim wezterm wget wine xclip xorg zoxide zoxide"
+
+function existCommand { command -v "$1" > /dev/null; }
+
+existCommand "paru"		|| (sudo pacman -S --needed base-devel && git clone https://aur.archlinux.org/paru.git $HOME/.local/builds/paru && cd $HOME/.local/builds/paru && makepkg -si && cd - > /dev/null)
+existCommand "paruz"	|| (git clone https://github.com/joehillen/paruz.git $HOME/.local/builds/paruz && cd $HOME/.local/builds/paruz && sudo make install && cd - > /dev/null)
+existCommand "nvm"		|| (PROFILE=/dev/null bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash')
+
+eval "paru -Syy $REQUIRED_PACMAN_PACKAGES"
+
+# Install node
+existCommand "nvm" && nvm install $NODE_VERSION
+
+existCommand "npm" && npm i -g neovim nodemon pnpm spottydl ts-node typescript yarn || printf "${RED}Command npm not found..."
+
---
+++ zsh/.config/zsh/functions.sh
@@ -0,0 +1,82 @@
+
+function editDir { (cd "$1" || exit) && $EDITOR . && (cd - > /dev/null || exit); }
+
+function existCommand { 
+	command -v "$1" > /dev/null
+}
+
+function botstrap_plugin {
+	if [ -z "$1" ] || [ -z "$2" ]; then
+		printf "usage:\n\t${BLUE}botstrap_plugin${NC} [name] [repo-url]\n"	
+	else
+		ZSH_PLUGIN_DIR=$ZSH_PLUGINS/$1
+		if [ ! -d "$ZSH_PLUGIN_DIR" ]; then
+			git clone "$2" "$ZSH_PLUGIN_DIR"
+		fi
+	fi
+}
+
+function botstrap_omzsh_plugin {
+  if [ -z "$1" ]; then
+    printf "usage: \n\t${BLUE}botstrap_plugin${NC} \"[name1] [name2] ...\" <dir>\n\t${BLUE}botstrap_plugin${NC} \"[name1],[name2],...\" <dir>\n"
+  else
+
+    if [ ! -z "$2" ]; then OMZ_PLUGIN_INSTALL_DIR=$2
+    else OMZ_PLUGIN_INSTALL_DIR=$ZSH_PLUGINS; fi
+
+      # echo $1 | tr " " "\n"
+      echo "$1" | tr "," "\n" | tr " " "\n" | while read -r plugin; do 
+      if [ ! -d "$OMZ_PLUGIN_INSTALL_DIR/$plugin" ]; then 
+        if [ ! -d "/tmp/omzh" ]; then
+          git clone https://github.com/ohmyzsh/ohmyzsh.git /tmp/omzh
+        fi
+        cp -r "/tmp/omzh/plugins/$plugin" "$OMZ_PLUGIN_INSTALL_DIR/"
+      fi
+    done
+  fi
+
+  if [ -d /tmp/omzh ]; then rm -rf /tmp/omzh; fi
+}
+
+function packageAdd {
+  (([ -z "$2" ] && existCommand "$1") || ([ ! -z "$2" ] && eval "$2 $1" > /dev/null)) || PACKAGES="$PACKAGES $1"
+}
+
+function bootstrapSubstitution {
+	APPS=$1
+	echo "$2" | tr " " "\n" | while read -r SUBST; do
+		APPS="$(echo "$APPS" | sed -e "s/$(echo "$SUBST" | cut -d '=' -f1)/$(echo "$SUBST" | cut -d '=' -f2 | tr ';' ' ')/g")"
+	done
+	printf "$APPS"
+	unset APPS
+}
+
+##
+#	$1 = PACKAGES
+#	$2 = SUBSTITUTIONS
+#	$3 = DOWNLOAD COMMAND
+#	$4 = METHOD TO FIND EXISTANCE
+##
+function generalPackageBootstrap {
+	PACKAGES=""
+	echo "$2" | tr " " "\n" | while read -r PACKET; do
+		packageAdd "$PACKET" "$4"
+	done
+  # FIX: broke when substitution is empty
+	[ ! -z "$PACKAGES" ] && print "Installing apps:$PACKAGES" && eval "$1$(bootstrapSubstitution "$PACKAGES" "$3")";
+	unset PACKAGES
+}
+
+function setCustomFuncs {
+  source $HOME/.config/zsh/functions.sh
+}
+
+function unsetCustomFuncs {
+	unset -f packageAdd;
+	unset -f bootstrapSubstitution;
+	unset -f generalPackageBootstrap;
+	unset -f botstrap_plugin;
+	unset -f botstrap_omzsh_plugin;
+  unset -f unsetCustomFuncs;
+}
+
