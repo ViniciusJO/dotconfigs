9353c4f216bc56d0b3db6a17c2297b4d64d06231
ViniciusJO <viniciusjosedeoliveira@outlook.com>
2024-07-10 09:52:12 -0300

    nvim oxocarbon theme

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,6 +1,7 @@
 return {
-  { 'wuelnerdotexe/vim-enfocado',       priority = 1000 },
-  { 'whatsthatsmell/codesmell_dark.vim' },
-  { 'catppuccin/nvim',                  name = 'catppuccin', priority = 1000 },
-  { 'rebelot/kanagawa.nvim',            priority = 1000 },
+  { 'wuelnerdotexe/vim-enfocado',         name='enfocado',          priority = 1000 },
+  { 'whatsthatsmell/codesmell_dark.vim',  name = 'codesmell_dark',  priority = 1000 },
+  { 'catppuccin/nvim',                    name = 'catppuccin',      priority = 1000 },
+  { 'rebelot/kanagawa.nvim',              name = 'kanegawa',        priority = 1000 },
+  { 'nyoom-engineering/oxocarbon.nvim',   name = 'oxocarbon',       priority = 1000 }
 }
